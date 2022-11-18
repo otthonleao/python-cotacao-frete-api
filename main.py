e53184aff04cbcccf330316567a901f65175c3d4
Otthon Leão <otthonleao@hotmail.com>
2022-11-18 17:08:21 -0300

    criação do método POST para enviar os dados de dimensão do produto as transportadoras

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+#Input com os dados do produto para o frete ser calculado
 class Item(BaseModel):
-	name: str
-	price: float
-	is_offer: Union[bool] = None
+	heightProduct: int
+	widthProduct: int
+	weightProduct: int
 
-@app.get("/")
-def read_root():
-	return {"Hello": "World"}
+@app.post("/dados-produto")
+def read_root(item: Item):
+	return {
+		"dimensao": {
+						"altura": item.heightProduct,
+						"largura": item.widthProduct
+					},
+		"peso": item.weightProduct
+	}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q:Union[str, None] = None):
-	return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q:Union[str, None] = None):
+# 	return {"item_id": item_id, "q": q}
 
-@app.put("/items/{item_id")
-def update_item(item_id: int, item: Item):
-	return {"item_name": item.name, "item_id": item_id}
+
+# @app.put("/items/{item_id")
+# def update_item(item_id: int, item: Item):
+# 	return {"item_name": item.name, "item_id": item_id}
